981a6c3c526bda8d094c557ae76061e3effcdce5
Shivn Agarwal <shivin.agarwal15@gmail.com>
2015-10-04 18:34:19 +0530

    Add edit and destroy episode

--- app/controllers/episods_controller.rb
+++ app/controllers/episods_controller.rb
@@ -1,7 +1,7 @@
 class EpisodsController < ApplicationController
 
 	before_action :find_podcast
-	before_action :find_episod, only: [:show]
+	before_action :find_episod, only: [:show, :edit, :update, :destroy]
 	
   	def new
 		@episod = @podcast.episod.new
@@ -18,6 +18,23 @@ class EpisodsController < ApplicationController
 
 	def show
 		@episods = Episod.where(podcast_id: @podcast).order("created_at DESC").reject {|e| e.id == @episod.id}
+	end
+
+	def edit
+	end
+
+	def update
+		if @episod.update episod_params
+			redirect_to podcast_episod_path(@podcast, @episod), notice: "Episode  Was Succfully Updated!"
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+
+		@episod.destroy
+		redirect_to root_path
 	end
 
 	private
